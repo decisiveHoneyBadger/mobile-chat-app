af8d31e621fd2728772a80ddfc53e1d0d269c22e
decisiveHoneyBadger <k.guevenc@web.de>
2022-07-30 12:27:12 +0200

    included GiftedChat as dependency

--- components/Chat.js
+++ components/Chat.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 const styles = StyleSheet.create({
   container: {
